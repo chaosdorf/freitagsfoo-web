f4709192b3b18244167d2c5c011e25559e1960c7
Niklas Sombert <niklas@ytvwld.de>
2018-08-27 15:50:03 +0200

    Add Bootstrap.

--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app.config["BOOTSTRAP_SERVE_LOCAL"] = True
+Bootstrap(app)
 
 
 @app.route("/")
