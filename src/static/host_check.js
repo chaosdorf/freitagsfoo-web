b1aba5c54bae5225e4fc82bcb81abe4d23489b2c
Niklas Sombert <niklas@ytvwld.de>
2018-08-31 19:30:02 +0200

    Add HTML for checking for Info-Beamer Pi.

---
+++ src/static/host_check.js
@@ -0,0 +1,73 @@
+"use strict";
+
+function doCheck() {
+    reset();
+    doCheckPi();
+}
+
+function reset() {
+    jQuery(".status").fadeOut();
+}
+
+function renderStatus(enabled_alerts, data) {
+    let rendered = Handlebars.templates.host_check(data);
+    jQuery("#check-status-container").html(rendered);
+    for(alert of enabled_alerts) {
+        jQuery("#"+alert).fadeIn();
+    }
+}
+
+function doCheckPi() {
+    jQuery.ajax("/host/check/info-beamer").always(function(data, status) {
+        if(status !== "success") {
+            handleNetworkError(status);
+            return;
+        }
+        if(data["status"] === "ok") {
+            renderStatus(["pi-success"], {
+                "pi-id": data["data"]["id"],
+                "pi-setup-name": data["data"]["setup"]["name"],
+                "pi-setup-id": data["data"]["setup"]["id"],
+            });
+        }
+        else if(data["status"] === "error") {
+            switch(data["last_step"]) {
+                case "info-beamer.com":
+                    renderStatus(["pi-error-info-beamer"], {});
+                    break;
+                case "find-device":
+                    renderStatus(["pi-error-device-missing"], {
+                        "pi-id": data["data"]["id"],
+                    });
+                    break;
+                case "check-device-online":
+                    renderStatus(["pi-error-device-offline"], {
+                        "pi-id": data["data"]["id"],
+                    });
+                    break;
+                case "check-device-synced":
+                    renderStatus(["pi-error-device-not-synced"], {
+                        "pi-id": data["data"]["id"],
+                    });
+                    break;
+                case "setup":
+                    renderStatus(["pi-error-device-wrong-setup"], {
+                        "pi-id": data["data"]["device"]["id"],
+                        "pi-setup-name": data["data"]["actual"]["name"],
+                        "pi-setup-id": data["data"]["actual"]["name"],
+                        "pi-expected-setup-name": data["data"]["expected"]["name"],
+                        "pi-expected-setup-id": data["data"]["expected"]["id"],
+                    });
+                    break;
+            }
+        }
+    })
+}
+
+function handleNetworkError(status) {
+    renderStatus(["network-error"], {
+        "network-error-reason": status
+    });
+}
+
+window.onload = doCheck;
