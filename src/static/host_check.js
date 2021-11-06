0f49f1916972ac3bd47ef7c504ca67dfc2e7e960
Niklas Sombert <niklas@ytvwld.de>
2021-11-06 01:18:42 +0100

    Port host_check from Handlebars to Vue

--- src/static/host_check.js
+++ src/static/host_check.js
@@ -1,109 +1,98 @@
 "use strict";
 
-function doCheck() {
-    reset();
-    doCheckPi();
-}
-
-function reset() {
-    jQuery("#recheck-button").prop("disabled", true);
-    jQuery(".status").fadeOut();
-}
-
-function renderStatus(enabled_alerts, data) {
-    let rendered = Handlebars.templates.host_check(data);
-    jQuery("#check-status-container").html(rendered);
-    for(alert of enabled_alerts) {
-        jQuery("#"+alert).fadeIn();
-    }
-}
-
-function doCheckPi() {
-    jQuery.ajax("/host/check/info-beamer").always(function(data, status) {
-        if(status !== "success") {
-            handleNetworkError(status);
-        }
-        else if(data["status"] === "ok") {
-            renderStatus(["pi-success"], {
-                "pi-id": data["data"]["id"],
-                "pi-setup-name": data["data"]["setup"]["name"],
-                "pi-setup-id": data["data"]["setup"]["id"],
+var checkApp = new Vue({
+    el: "#check-status",
+    data: {
+        checking: true,
+        piId: null,
+        piSetupName: null,
+        piSetupId: null,
+        piExpectedSetupName: null,
+        piExpectedSetupId: null,
+        piErrors: [],
+        piWarnings: [],
+        networkErrors: []
+    },
+    methods: {
+        doCheck: function() {
+            this.reset();
+            this.doCheckPi();
+        },
+        doCheckPi: function() {
+            jQuery.ajax("/host/check/info-beamer").always(this.checkPiCallback);
+        },
+        checkPiCallback: function(data, status) {
+            if(status !== "success") {
+                this.networkErrors.push(status);
+            }
+            else if(data["status"] === "ok") {
+                this.piId = data["data"]["id"];
+                this.piSetupName = data["data"]["setup"]["name"];
+                this.piSetupId = data["data"]["setup"]["id"];
+            }
+            else if(data["status"] === "error") {
+                switch(data["last_step"]) {
+                    case "info-beamer.com":
+                        this.piErrors.push("infoBeamer");
+                        break;
+                    case "find-device":
+                        this.piErrors.push("deviceMissing");
+                        this.piId = data["data"]["id"];
+                        break;
+                    case "setup":
+                        this.piErrors.push("deviceWrongSetup");
+                        this.piId = data["data"]["device"]["id"];
+                        this.piSetupName = data["data"]["actual"]["name"];
+                        this.piSetupId = data["data"]["actual"]["id"];
+                        this.piExpectedSetupName = data["data"]["expected"]["name"];
+                        this.piExpectedSetupId = data["data"]["expected"]["id"];
+                        break;
+                    case "check-device-online":
+                        this.piWarnings.push("deviceOffline");
+                        this.piId = data["data"]["id"];
+                        this.piSetupName = data["data"]["setup"]["name"];
+                        this.piSetupId = data["data"]["setup"]["id"];
+                        break;
+                    case "check-device-synced":
+                        this.piWarnings.push("deviceNotSynced");
+                        this.piId = data["data"]["id"];
+                        this.piSetupName = data["data"]["setup"]["name"];
+                        this.piSetupId = data["data"]["setup"]["id"];
+                        break;
+                }
+            }
+            this.checking = false;
+        },
+        piChangeSetup: function() {
+            jQuery("#pi-error-device-wrong-setup").fadeOut();
+            jQuery.ajax("/host/check/info-beamer", {method: "POST"}).always(function(data, status){
+                if(status !== "success") {
+                    this.networkErrors.push(status);
+                }
+                else if(data["status"] === "ok") {
+                    this.doCheckPi();
+                }
+                else if(data["status"] === "error") {
+                    switch(data["last_step"]) {
+                        case "info-beamer.com":
+                            this.piErrors.push("infoBeamer");
+                            break;
+                        case "assign-setup":
+                            this.piErrors.push("assignSetup");
+                            this.piId = data["data"]["device"]["id"];
+                            this.piSetupName = data["data"]["actual"]["name"];
+                            this.piSetupId = data["data"]["actual"]["id"];
+                    }
+                }
             });
+        },
+        reset: function() {
+            this.checking = true;
+            this.piErrors = [];
+            this.piWarnings = [];
+            this.networkErrors = [];
         }
-        else if(data["status"] === "error") {
-            switch(data["last_step"]) {
-                case "info-beamer.com":
-                    renderStatus(["pi-error-info-beamer"], {});
-                    break;
-                case "find-device":
-                    renderStatus(["pi-error-device-missing"], {
-                        "pi-id": data["data"]["id"],
-                    });
-                    break;
-                case "setup":
-                    renderStatus(["pi-error-device-wrong-setup"], {
-                        "pi-id": data["data"]["device"]["id"],
-                        "pi-setup-name": data["data"]["actual"]["name"],
-                        "pi-setup-id": data["data"]["actual"]["id"],
-                        "pi-expected-setup-name": data["data"]["expected"]["name"],
-                        "pi-expected-setup-id": data["data"]["expected"]["id"],
-                    });
-                    break;
-                case "check-device-online":
-                    renderStatus(
-                        ["pi-success", "pi-warning-device-offline"],
-                        {
-                            "pi-id": data["data"]["id"],
-                            "pi-setup-name": data["data"]["setup"]["name"],
-                            "pi-setup-id": data["data"]["setup"]["id"],
-                        }
-                    );
-                    break;
-                case "check-device-synced":
-                    renderStatus(
-                        ["pi-success", "pi-warning-device-not-synced"],
-                        {
-                            "pi-id": data["data"]["id"],
-                            "pi-setup-name": data["data"]["setup"]["name"],
-                            "pi-setup-id": data["data"]["setup"]["id"],
-                        }
-                    );
-                    break;
-            }
-        }
-        jQuery("#recheck-button").prop("disabled", false);
-    })
-}
+    }
+});
 
-function piChangeSetup() {
-    jQuery("#pi-error-device-wrong-setup").fadeOut();
-    jQuery.ajax("/host/check/info-beamer", {method: "POST"}).always(function(data, status){
-        if(status !== "success") {
-            handleNetworkError(status);
-        }
-        else if(data["status"] === "ok") {
-            doCheckPi();
-        }
-        else if(data["status"] === "error") {
-            switch(data["last_step"]) {
-                case "info-beamer.com":
-                    renderStatus(["pi-error-info-beamer"], {});
-                    break;
-                case "assign-setup":
-                    renderStatus(["pi-error-assign-setup"], {
-                      "pi-id": data["data"]["device"]["id"],
-                      "pi-setup-name": data["data"]["actual"]["name"],
-                      "pi-setup-id": data["data"]["actual"]["id"],
-                    });
-            }
-        }
-    });
-}
-
-function handleNetworkError(status) {
-    renderStatus(["network-error"], {
-        "network-error-reason": status
-    });
-}
-
-window.onload = doCheck;
+window.onload = checkApp.doCheck;
