chaosdorf/freitagsfoo-web

commit 74a5157930f5b64983bb6dcd19785bf1c4db7656

Remove Handlebars

Dockerfile
@@ -6,10 +6,6 @@ RUN mkdir -p ./src
 COPY package.json ./
 COPY yarn.lock ./
 RUN cd /opt/app && yarn
-COPY src/client_templates ./src/client_templates
-RUN mkdir -p ./src/static/templates \
-    && cd ./src/client_templates \
-    && make
 
 
 FROM python:3.7-alpine

src/client_templates/Makefile
@@ -1,7 +1,0 @@
-all: Makefile
-
-../static/templates/%.min.js: %.handlebars
-	../../node_modules/.bin/handlebars $< --output $@ --min
-
-../static/templates/%.js: %.handlebars
-	../../node_modules/.bin/handlebars $< --output $@

src/static/handlebars.runtime.js
@@ -1,1 +1,0 @@
-../../node_modules/handlebars/dist/handlebars.runtime.js

src/templates/base.html.j2
@@ -16,7 +16,6 @@
             }).install();
         </script>
         {% endif %}
-        <script type="text/javascript" src="/static/handlebars.runtime.min.js"></script>
         <script type="text/javascript" src="{{ url_for('static', filename='vue.min.js') }}"></script>
         <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
             <a class="navbar-brand" href="#">

commit 6907261efb0f94a7b379cdb94134613b93b68323

Fix Docker build
This regression was introduced in 74a5157930f5b64983bb6dcd19785bf1c4db7656.

Dockerfile
@@ -18,7 +18,6 @@ COPY Pipfile.lock ./
 RUN pipenv install --system --deploy
 COPY . ./
 COPY --from=Builder /opt/app/node_modules ./node_modules
-COPY --from=Builder /opt/app/src/static/templates ./src/static/templates
 COPY config.default.cfg /etc/freitagsfoo-web.cfg
 
 ENV CONFIG_FILE /etc/freitagsfoo-web.cfg