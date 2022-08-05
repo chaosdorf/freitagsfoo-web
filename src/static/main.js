298eb9f35911b880a34fdd7ac907b0370f1977e0
Niklas Sombert <niklas@ytvwld.de>
2022-08-05 18:31:28 +0200

    Disable popovers for now

--- src/static/main.js
+++ src/static/main.js
@@ -1,11 +1,1 @@
 Vue.use(BootstrapVue);
-
-
-
-// initialize pop-overs
-jQuery(function () {
-    jQuery('[data-toggle="popover"]').popover({
-        trigger: "focus",
-        html: true
-    });
-})
