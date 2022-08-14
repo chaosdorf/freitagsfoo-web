09afc6459794daae34c83618ef36c59a8cf238cc
Niklas Sombert <niklas@ytvwld.de>
2022-08-14 21:05:08 +0200

    Fully remove jQuery

--- src/static/host_check.js
+++ src/static/host_check.js
@@ -14,134 +14,149 @@ var checkApp = new Vue({
         networkErrors: []
     },
     methods: {
+        handleNetworkError: function (error) {
+            this.networkErrors.push(error);
+        },
         doCheck: function() {
             this.reset();
             this.doCheckPi();
         },
         doCheckPi: function() {
-            jQuery.ajax("/host/check/info-beamer").always(this.checkPiCallback);
+            fetch("/host/check/info-beamer")
+            .then(this.checkPiCallback, this.handleNetworkError);
         },
-        checkPiCallback: function(data, status) {
-            if(status !== "success") {
-                this.networkErrors.push(status);
-            }
-            else if(data["status"] === "ok") {
-                this.piId = data["data"]["id"];
-                this.piSetupName = data["data"]["setup"]["name"];
-                this.piSetupId = data["data"]["setup"]["id"];
-            }
-            else if(data["status"] === "error") {
-                switch(data["last_step"]) {
-                    case "info-beamer.com":
-                        this.piErrors.push("infoBeamer");
-                        break;
-                    case "find-device":
-                        this.piErrors.push("deviceMissing");
-                        this.piId = data["data"]["id"];
-                        break;
-                    case "setup":
-                        this.piErrors.push("deviceWrongSetup");
-                        this.piId = data["data"]["device"]["id"];
-                        this.piSetupName = data["data"]["actual"]["name"];
-                        this.piSetupId = data["data"]["actual"]["id"];
-                        this.piExpectedSetupName = data["data"]["expected"]["name"];
-                        this.piExpectedSetupId = data["data"]["expected"]["id"];
-                        break;
-                    case "check-device-online":
-                        this.piWarnings.push("deviceOffline");
+        checkPiCallback: function(response) {
+            if(!response.ok) {
+                this.networkErrors.push(response.status);
+            } else {
+                response.json().then((data) => {
+                    if(data["status"] === "ok") {
                         this.piId = data["data"]["id"];
                         this.piSetupName = data["data"]["setup"]["name"];
                         this.piSetupId = data["data"]["setup"]["id"];
-                        break;
-                    case "check-device-synced":
-                        this.piWarnings.push("deviceNotSynced");
-                        this.piId = data["data"]["id"];
-                        this.piSetupName = data["data"]["setup"]["name"];
-                        this.piSetupId = data["data"]["setup"]["id"];
-                        break;
-                    case "state":
-                        this.piWarnings.push("stateUnknown");
-                        this.piId = data["data"]["id"];
-                        this.piSetupName = data["data"]["setup"]["name"];
-                        this.piSetupId = data["data"]["setup"]["id"];
-                        break;
-                    case "extron":
-                        this.piWarnings.push("extronWrongInput");
-                        this.piId = data["data"]["id"];
-                        this.piSetupName = data["data"]["setup"]["name"];
-                        this.piSetupId = data["data"]["setup"]["id"];
-                        break;
-                }
+                    } else if(data["status"] === "error") {
+                        switch(data["last_step"]) {
+                            case "info-beamer.com":
+                                this.piErrors.push("infoBeamer");
+                                break;
+                            case "find-device":
+                                this.piErrors.push("deviceMissing");
+                                this.piId = data["data"]["id"];
+                                break;
+                            case "setup":
+                                this.piErrors.push("deviceWrongSetup");
+                                this.piId = data["data"]["device"]["id"];
+                                this.piSetupName = data["data"]["actual"]["name"];
+                                this.piSetupId = data["data"]["actual"]["id"];
+                                this.piExpectedSetupName = data["data"]["expected"]["name"];
+                                this.piExpectedSetupId = data["data"]["expected"]["id"];
+                                break;
+                            case "check-device-online":
+                                this.piWarnings.push("deviceOffline");
+                                this.piId = data["data"]["id"];
+                                this.piSetupName = data["data"]["setup"]["name"];
+                                this.piSetupId = data["data"]["setup"]["id"];
+                                break;
+                            case "check-device-synced":
+                                this.piWarnings.push("deviceNotSynced");
+                                this.piId = data["data"]["id"];
+                                this.piSetupName = data["data"]["setup"]["name"];
+                                this.piSetupId = data["data"]["setup"]["id"];
+                                break;
+                            case "state":
+                                this.piWarnings.push("stateUnknown");
+                                this.piId = data["data"]["id"];
+                                this.piSetupName = data["data"]["setup"]["name"];
+                                this.piSetupId = data["data"]["setup"]["id"];
+                                break;
+                            case "extron":
+                                this.piWarnings.push("extronWrongInput");
+                                this.piId = data["data"]["id"];
+                                this.piSetupName = data["data"]["setup"]["name"];
+                                this.piSetupId = data["data"]["setup"]["id"];
+                                break;
+                        }
+                    }
+                });
             }
             this.checking = false;
         },
         piChangeSetup: function() {
             this.reset();
-            jQuery.ajax("/host/check/info-beamer", {method: "POST"})
-            .always(this.piChangeSetupCallback);
+            fetch("/host/check/info-beamer", {method: "POST"})
+            .then(this.piChangeSetupCallback, this.handleNetworkError);
         },
-        piChangeSetupCallback: function(data, status){
-            if(status !== "success") {
-                this.networkErrors.push(status);
-            }
-            else if(data["status"] === "ok") {
-                this.doCheckPi();
-            }
-            else if(data["status"] === "error") {
-                switch(data["last_step"]) {
-                    case "info-beamer.com":
-                        this.piErrors.push("infoBeamer");
-                        break;
-                    case "assign-setup":
-                        this.piErrors.push("assignSetup");
-                        this.piId = data["data"]["device"]["id"];
-                        this.piSetupName = data["data"]["actual"]["name"];
-                        this.piSetupId = data["data"]["actual"]["id"];
-                }
+        piChangeSetupCallback: function(response){
+            if(!response.ok) {
+                this.networkErrors.push(response.status);
+            } else {
+                response.json().then((data) => {
+                    if(data["status"] === "ok") {
+                        this.doCheckPi();
+                    }
+                    else if(data["status"] === "error") {
+                        switch(data["last_step"]) {
+                            case "info-beamer.com":
+                                this.piErrors.push("infoBeamer");
+                                break;
+                            case "assign-setup":
+                                this.piErrors.push("assignSetup");
+                                this.piId = data["data"]["device"]["id"];
+                                this.piSetupName = data["data"]["actual"]["name"];
+                                this.piSetupId = data["data"]["actual"]["id"];
+                        }
+                    }
+                });
             }
             this.checking = false;
         },
         piResetState: function() {
             this.reset();
-            jQuery.ajax("/host/check/info-beamer", {method: "PATCH"})
-            .always(this.piResetStateCallback);
+            fetch("/host/check/info-beamer", {method: "PATCH"})
+            .then(this.piResetStateCallback, this.handleNetworkError);
         },
-        piResetStateCallback: function(data, status) {
-            if(status !== "success") {
-                this.networkErrors.push(status);
-            }
-            else if(data["status"] === "ok") {
-                this.doCheckPi();
-            }
-            else if(data["status"] === "error") {
-                switch(data["last_step"]) {
-                    case "info-beamer.com":
-                        this.piErrors.push("infoBeamer");
-                        break;
-                    case "send-command":
-                        this.piErrors.push("resetState");
-                }
+        piResetStateCallback: function(response) {
+            if(!response.ok) {
+                this.networkErrors.push(response.status);
+            } else {
+                response.json().then((data) => {
+                    if(data["status"] === "ok") {
+                        this.doCheckPi();
+                    }
+                    else if(data["status"] === "error") {
+                        switch(data["last_step"]) {
+                            case "info-beamer.com":
+                                this.piErrors.push("infoBeamer");
+                                break;
+                            case "send-command":
+                                this.piErrors.push("resetState");
+                        }
+                    }
+                });
             }
             this.checking = false;
         },
         extronSwitchPort: function() {
             this.reset();
-            jQuery.ajax("/host/check/info-beamer", {method: "PUT"})
-            .always(this.extronSwitchPortCallback);
+            fetch("/host/check/info-beamer", {method: "PUT"})
+            .then(this.extronSwitchPortCallback, this.handleNetworkError);
         },
-        extronSwitchPortCallback: function(data, status) {
-            if (status !== "success") {
-                this.networkErrors.push(status);
-            }
-            else if (data["status"] === "ok") {
-                this.doCheckPi();
-            }
-            else if (data["status"] === "error") {
-                switch (data["last_step"]) {
-                    case "extron":
-                        this.piErrors.push("extron");
-                        break;
-                }
+        extronSwitchPortCallback: function(response) {
+            if (!response.ok) {
+                this.networkErrors.push(response.status);
+            } else {
+                response.json().then((data) => {
+                    if (data["status"] === "ok") {
+                        this.doCheckPi();
+                    }
+                    else if (data["status"] === "error") {
+                        switch (data["last_step"]) {
+                            case "extron":
+                                this.piErrors.push("extron");
+                                break;
+                        }
+                    }
+                });
             }
             this.checking = false;
         },
